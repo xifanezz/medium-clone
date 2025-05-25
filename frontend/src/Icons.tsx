c1958ac0c064f2161f98a14506c41beb70414364
Sumit Bhuia <sumitbhuia100@gmail.com>
2025-05-26 02:40:27 +0530

    chore: add svgs code modularity

--- frontend/src/Icons.tsx
+++ frontend/src/Icons.tsx
@@ -148,134 +148,213 @@ export const X = ({ size = 16, color = "currentColor" }) => (
 );
 
 export const BulletList = ({ size = 16, color = "currentColor" }) => (
-    <svg 
-    xmlns="http://www.w3.org/2000/svg" 
-    fill="none" 
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
     width={size}
     height={size}
-    viewBox="0 0 24 24" 
-    strokeWidth="2" 
-    stroke={color} 
-    >
-  <path 
-  strokeLinecap="round" 
-  strokeLinejoin="round" 
-  d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" 
-  />
-</svg>
+    viewBox="0 0 24 24"
+    strokeWidth="2"
+    stroke={color}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+    />
+  </svg>
 );
 
 export const OrderedList = ({ size = 16, color = "currentColor" }) => (
-    <svg 
-    xmlns="http://www.w3.org/2000/svg" 
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
     width={size}
     height={size}
-    fill="none" 
-    viewBox="0 0 24 24" 
-    strokeWidth="2" 
-    stroke={color} 
-    >
-  <path 
-  strokeLinecap="round" 
-  strokeLinejoin="round" 
-  d="M8.242 5.992h12m-12 6.003H20.24m-12 5.999h12M4.117 7.495v-3.75H2.99m1.125 3.75H2.99m1.125 0H5.24m-1.92 2.577a1.125 1.125 0 1 1 1.591 1.59l-1.83 1.83h2.16M2.99 15.745h1.125a1.125 1.125 0 0 1 0 2.25H3.74m0-.002h.375a1.125 1.125 0 0 1 0 2.25H2.99" 
-  />
-</svg>
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth="2"
+    stroke={color}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M8.242 5.992h12m-12 6.003H20.24m-12 5.999h12M4.117 7.495v-3.75H2.99m1.125 3.75H2.99m1.125 0H5.24m-1.92 2.577a1.125 1.125 0 1 1 1.591 1.59l-1.83 1.83h2.16M2.99 15.745h1.125a1.125 1.125 0 0 1 0 2.25H3.74m0-.002h.375a1.125 1.125 0 0 1 0 2.25H2.99"
+    />
+  </svg>
 
 );
 
-export  const Options  = ({ size = 6, color = "currentColor" }) => (
-
-    <svg 
-    xmlns="http://www.w3.org/2000/svg" 
-    fill="none" 
-    viewBox="0 0 24 24" 
-    strokeWidth="1.5" 
-    stroke={color} 
+export const Bell = ({ size = 16, color = "currentColor" }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
     width={size}
     height={size}
-    className="size-6 mr-4" // The size wont work because clssName is already defined in the component
-    >
-    <path 
-    strokeLinecap="round" 
-    strokeLinejoin="round" d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-    </svg>
-);
-
-export const Bell = ({ size = 16, color = "currentColor" }) => (    
-    <svg
-    xmlns="http://www.w3.org/2000/svg" 
-    width={size}
-    height={size}
-    fill="none" 
-    viewBox="0 0 24 24" 
-    strokeWidth="1.5" 
-    stroke={color} 
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth="1.5"
+    stroke={color}
     className="size-6 mr-4"// The size wont work because clssName is already defined in the component
-    >
-    <path 
-    strokeLinecap="round" 
-    strokeLinejoin="round" 
-    d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
-    </svg>
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
+  </svg>
 );
 
-export  const Quote = ({ size = 12, color = "currentColor" }) => (
-  <svg 
-  fill="#000000" 
-  xmlns="http://www.w3.org/2000/svg" 
-  stroke={color} 
-  width={size}
-  height={size} 
-   viewBox="0 0 123.961 123.961"
-	 >
-<g>
-	<path
-      strokeLinecap="round" 
-      strokeLinejoin="round" 
-   d="M49.8,29.032c3.1-1.3,4.4-5,3-8l-4.9-10.3c-1.4-2.899-4.8-4.2-7.8-2.899c-8.5,3.6-15.8,8.3-21.6,14
+export const Quote = ({ size = 12, color = "currentColor" }) => (
+  <svg
+    fill="#000000"
+    xmlns="http://www.w3.org/2000/svg"
+    stroke={color}
+    width={size}
+    height={size}
+    viewBox="0 0 123.961 123.961"
+  >
+    <g>
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M49.8,29.032c3.1-1.3,4.4-5,3-8l-4.9-10.3c-1.4-2.899-4.8-4.2-7.8-2.899c-8.5,3.6-15.8,8.3-21.6,14
 		C11.4,28.532,6.6,36.232,4,44.732c-2.6,8.601-4,20.3-4,35.2v30.7c0,3.3,2.7,6,6,6h39.3c3.3,0,6-2.7,6-6v-39.3c0-3.301-2.7-6-6-6
 		H26.5c0.2-10.101,2.6-18.2,7-24.301C37.1,36.133,42.5,32.133,49.8,29.032z"/>
-	<path
-      strokeLinecap="round" 
-      strokeLinejoin="round" 
-   d="M120.4,29.032c3.1-1.3,4.399-5,3-8l-4.9-10.199c-1.4-2.9-4.8-4.2-7.8-2.9c-8.4,3.6-15.601,8.3-21.5,13.9
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M120.4,29.032c3.1-1.3,4.399-5,3-8l-4.9-10.199c-1.4-2.9-4.8-4.2-7.8-2.9c-8.4,3.6-15.601,8.3-21.5,13.9
 		c-7.101,6.8-12,14.5-14.601,23c-2.6,8.399-3.899,20.1-3.899,35.1v30.7c0,3.3,2.7,6,6,6H116c3.3,0,6-2.7,6-6v-39.3
 		c0-3.301-2.7-6-6-6H97.1c0.2-10.101,2.601-18.2,7-24.301C107.7,36.133,113.1,32.133,120.4,29.032z"/>
-</g>
-</svg>
+    </g>
+  </svg>
 );
 
 export const Create = ({ size = 16, color = "currentColor" }) => (
-  <svg 
-  xmlns="http://www.w3.org/2000/svg" 
-  fill="none" 
-  viewBox="0 0 24 24" 
-  strokeWidth="1.5" 
-  stroke={color} 
-  width={size}
-  height={size}
-  className="size-6"
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth="1.5"
+    stroke={color}
+    width={size}
+    height={size}
+    className="size-6"
   >
-  <path 
-  strokeLinecap="round" 
-  strokeLinejoin="round" 
-  d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
   </svg>
 );
 
 export const Error = ({ size = 16, color = "currentColor" }) => (
-<svg 
-className="flex-shrink-0 inline w-4 h-4 me-3" 
-aria-hidden="true" 
-xmlns="http://www.w3.org/2000/svg" 
-stroke={color} 
-width={size}
-height={size}
-fill="currentColor" 
-viewBox="0 0 20 20"
->
-<path 
-d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+  <svg
+    className="flex-shrink-0 inline w-4 h-4 me-3"
+    aria-hidden="true"
+    xmlns="http://www.w3.org/2000/svg"
+    stroke={color}
+    width={size}
+    height={size}
+    fill="currentColor"
+    viewBox="0 0 24 24"
+  >
+    <path
+      d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+  </svg>
+);
+
+export const Options = ({ size = 16, color = "currentColor", ...props }) => (
+
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth="1.5"
+    stroke={color}
+    width={size}
+    height={size}
+    {...props}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round" d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+  </svg>
+);
+
+
+
+export const Save = ({ size = 16, color = "currentColor" ,...props}) => (
+  <svg 
+    aria-hidden="true"
+    xmlns="http://www.w3.org/2000/svg"
+    stroke= "currentColor"
+    width={size}
+    height={size}
+    fill={color}
+    viewBox="0 0 24 24"
+    {...props}
+  >
+<path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+  </svg>
+);
+
+
+export const Stop = ({ size = 16, color = "currentColor" , ...props}) => (
+  <svg
+    aria-hidden="true"
+    xmlns="http://www.w3.org/2000/svg"
+    stroke= "currentColor"
+    width={size}
+    height={size}
+    fill={color}
+    viewBox="0 0 24 24"
+    {...props}
+  >
+<path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
 </svg>
+);
+
+// currentColor is a defaulr attribute from svg
+
+// From Remix Icons
+
+// PlusIcon (unchanged)
+export const PlusIcon = ({ size = 24, color = "black", ...props }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill={color}
+    width={size}
+    height={size}
+    {...props}
+  >
+    <path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM11 11H7V13H11V17H13V13H17V11H13V7H11V11Z"></path>
+  </svg>
+);
+
+// PencilIcon (unchanged)
+export const PencilIcon = ({ size = 24, color = "black", ...props }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill={color}
+    width={size}
+    height={size}
+    {...props}
+  >
+    <path d="M15.7279 9.57627L14.3137 8.16206L5 17.4758V18.89H6.41421L15.7279 9.57627ZM17.1421 8.16206L18.5563 6.74785L17.1421 5.33363L15.7279 6.74785L17.1421 8.16206ZM7.24264 20.89H3V16.6473L16.435 3.21231C16.8256 2.82179 17.4587 2.82179 17.8492 3.21231L20.6777 6.04074C21.0682 6.43126 21.0682 7.06443 20.6777 7.45495L7.24264 20.89Z"></path>
+  </svg>
+);
+
+// SignOutIcon (new)
+export const SignOutIcon = ({ size = 24, color = "black", ...props }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill={color}
+    width={size}
+    height={size}
+    {...props}
+  >
+    <path d="M4 18H6V20H18V4H6V6H4V3C4 2.44772 4.44772 2 5 2H19C19.5523 2 20 2.44772 20 3V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V18ZM6 11H13V13H6V16L1 12L6 8V11Z"></path>
+  </svg>
 );
